3fa37b12a89b6b31af6b96f84f34979d7edfad5f
Vasyl <vasylchaban12@gmail.com>
2022-09-14 20:10:46 +0300

    building this programm in OOP principle

--- index.js
+++ index.js
@@ -5,43 +5,64 @@
    Web app: https://conversee12.github.io/a-tiny-JS-world/
    */
 
-const man = {
-   species: 'human',
-   name: 'Vasyl',
-   gender: 'male',
-   legs: 2,
-   hands: 2,
-   saying: 'Nothing can stop an idea whose time has come!',
-};
+class Inhabitant {
+   constructor(species, name, gender, saying) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.saying = saying;
+   }
+   get tinyWorld() {
+      return `${this.species}; ${this.name}; ${this.gender}; ${this.saying}`;
+   }
+}
 
-const woman = {
-   species: 'human',
-   name: 'Oksana',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'What\'s up?',
-};
+class Human extends Inhabitant {
+   constructor(name, gender, saying) {
+      super('human', name, gender, saying);
+      this.hands = 2;
+      this.legs = 2;
+   }
+   get tinyWorld() {
+      return `${this.species}; ${this.name}; ${this.gender}; ${this.hands}; ${this.legs}; ${this.saying}`;
+   }
+}
 
-const dog = {
-   species: 'dog',
-   name: 'Reks',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Hav-Hav',
-};
+class Animal extends Inhabitant {
+   constructor(species, name, gender, saying) {
+      super(species, name, gender, saying);
+      this.paws = 4;
+   }
+   get tinyWorld() {
+      return `${this.species}; ${this.name}; ${this.gender}; ${this.paws}; ${this.saying}`;
+   }
+}
 
-const cat = {
-   species: 'cat',
-   name: 'Pushok',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Meow',
-};
-const inhabitants = [man, woman, dog, cat];
+class Man extends Human {
+   constructor(name, saying) {
+      super(name, 'male', saying);
+   }
+}
 
-const inhabitantDescription = ["species", "name", "gender", "legs", "hands", "saying"];
+class Woman extends Human {
+   constructor(name, saying) {
+      super(name, 'female', saying);
+   }
+}
 
-inhabitants.forEach(residents => print(inhabitantDescription.map(property => residents[property]).join("; ")));
+class Cat extends Animal {
+   constructor(name, gender) {
+      super('cat', name, gender, 'Meow');
+   }
+}
+
+class Dog extends Animal {
+   constructor(name, gender) {
+      super('dog', name, gender, 'Hav-Hav');
+   }
+}
+
+const inhabitants = [new Man("Vasyl", "Nothing can stop an idea whose time has come!"), new Woman("Oksana", "What's up?"),
+new Cat("Pushok", "male"), new Dog("Reks", "male")];
+
+inhabitants.forEach(residents => print(residents.tinyWorld));
